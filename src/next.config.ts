fcafea11fc3af50bc01b5920376cc3f42b3fbffb
Eliabelmx <eliabe00x1@gmail.com>
2025-10-20 18:07:16 +0000

    studio-7929482140:~/studio{projeto}$ git commit -m "fix: Corrige configu

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,47 +1,9 @@
 
+
 import type {NextConfig} from 'next';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-const cspHeader = `
-      default-src 'self';
-      script-src 'self' ${isDevelopment ? "'unsafe-eval'" : ''} 'unsafe-inline' https://js.stripe.com https://fonts.googleapis.com;
-      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-      img-src 'self' blob: data: https://firebasestorage.googleapis.com https://i.imgur.com https://placehold.co;
-      font-src 'self' https://fonts.gstatic.com;
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      connect-src 'self' https://firestore.googleapis.com wss://firestore.googleapis.com https://www.googleapis.com https://identitytoolkit.googleapis.com https://api.stripe.com https://fonts.googleapis.com;
-    `.replace(/\s{2,}/g, ' ').trim();
-
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader,
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -84,3 +46,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
